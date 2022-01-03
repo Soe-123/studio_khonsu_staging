e60b456f4581217b904c294d099d3162754a3cd0
Soe-123 <82047482+Soe-123@users.noreply.github.com>
2022-01-03 14:18:19 +0000

    Peter and Adams Corections

--- slideshow.js
+++ slideshow.js
@@ -20,7 +20,41 @@ function showSlides(n) {
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
-  
+    
   }
   slides[slideIndex - 1].classList.add("active");
+};
+
+//image slider autoplay
+var playSlider;
+
+var repeater = () => {
+  playSlider = setInterval(function(){
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if(slideNumber > (numberOfSlides - 1)){
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  }, 4000);
 }
+repeater();
+
+//stop the image slider autoplay on mouseover
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+//start the image slider autoplay again on mouseout
+slider.addEventListener("mouseout", () => {
+  repeater();
+});
